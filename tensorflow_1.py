048748db3cda2bcb1bc9de72a716673b95cef9dd
Sancho McCann <sanchom@gmail.com>
2017-02-21 10:10:04 -0800

    Minimalization. More descriptive comments. More encapsulation.

--- tensorflow_1.py
+++ tensorflow_1.py
@@ -8,43 +8,14 @@ import shutil
 import numpy as np
 import tensorflow as tf
 
-def inputs():
-    # Defining the input placeholders
-    with tf.variable_scope('inputs'):
-        x = tf.placeholder(tf.float32, shape=(None, 2)) # 2-d vector
-        y = tf.placeholder(tf.float32, shape=(None, 1)) # scalar
-        return x, y
-
-def inference(x):
-    with tf.variable_scope('fully_connected'):
-        w = tf.Variable(tf.random_normal([2,1]))
-        tf.summary.histogram('w', w)
-        b = tf.Variable(tf.constant(0.0))
-        tf.summary.histogram('b', b)
-        return tf.nn.sigmoid(tf.matmul(x, w) + b)
-
-def loss(y, y_hat):
-    with tf.variable_scope('loss'):
-        loss_op = tf.losses.log_loss(y, y_hat)
-        tf.summary.scalar('loss', loss_op)
-        return loss_op
-
-global_step = tf.Variable(0, name='global_step', trainable=False)
-
-x, y = inputs()        
-y_hat = inference(x)
-loss_op = loss(y, y_hat)
-with tf.variable_scope('optimizer'):
-    rate = tf.train.exponential_decay(0.01, global_step, 1000, 0.97)
-    tf.summary.scalar('learning_rate', rate)
-    optimize_op = tf.train.AdamOptimizer(learning_rate=rate).minimize(
-        loss_op,
-        global_step=global_step)
-
 class Data(object):
     def __init__(self):
         self.index = 0
         self.data = []
+        with open('dummy_data.txt') as f:
+            for line in f:
+                self.data.append(tuple([float(t) for t in line.split()]))
+
     def get_next_batch(self, batch_size):
         x = np.zeros((batch_size,2), np.float32)
         y = np.zeros((batch_size,1), np.float32)
@@ -55,22 +26,58 @@ class Data(object):
             self.index = (self.index + 1) % len(self.data)
         return x, y
 
-data = Data()
+def inputs():
+    # Defining the input placeholders.
+    with tf.variable_scope('inputs'):
+        x = tf.placeholder(tf.float32, shape=(None, 2)) # 2-d vector
+        y = tf.placeholder(tf.float32, shape=(None, 1)) # scalar
+        return x, y
 
-with open('dummy_data.txt') as f:
-    for line in f:
-        line = line.strip()
-        [_x1, _x2, _y] = line.split()
-        data.data.append((float(_x1), float(_x2), float(_y)))
+def inference(x):
+    # Defining the inference graph and associated summaries.
+    with tf.variable_scope('fully_connected'):
+        w = tf.Variable(tf.random_normal([2,1]))
+        tf.summary.histogram('w', w)
+        b = tf.Variable(tf.constant(0.0))
+        tf.summary.histogram('b', b)
+        return tf.nn.sigmoid(tf.matmul(x, w) + b)
+
+def loss(y, y_hat):
+    # Defining the loss graph.
+    with tf.variable_scope('loss'):
+        loss_op = tf.losses.log_loss(y, y_hat)
+        tf.summary.scalar('loss', loss_op)
+        return loss_op
+
+def optimizer(loss_op, global_step):
+    with tf.variable_scope('optimizer'):
+        rate = tf.train.exponential_decay(0.01, global_step, 1000, 0.97)
+        tf.summary.scalar('learning_rate', rate)
+        optimize_op = tf.train.AdamOptimizer(learning_rate=rate).minimize(
+            loss_op,
+            global_step=global_step)
+        return optimize_op
+
+# Define the global step and its initialization.
+global_step = tf.Variable(0, name='global_step', trainable=False)
+
+# Putting the graph together
+x, y = inputs()
+y_hat = inference(x)
+loss_op = loss(y, y_hat)
+optimization_op = optimizer(loss_op, global_step)
+
+data = Data()
 
 shutil.rmtree('/tmp/tensorflow_1', ignore_errors=True)
 
-# Automatically handles initialization, summary writing, etc.
+# MonitoredTrainingSession automatically handles global variable
+# initialization, summary writing, checkpoints, watching for stopping
+# criteria, etc.
 with tf.train.MonitoredTrainingSession(
         checkpoint_dir="/tmp/tensorflow_1",
-        hooks=[tf.train.StopAtStepHook(last_step=100000)],
-        config=tf.ConfigProto()
+        hooks=[tf.train.StopAtStepHook(last_step=100000)]
 ) as sess:
     while not sess.should_stop():
         input_x, input_y = data.get_next_batch(100)
-        sess.run(optimize_op, feed_dict={x: input_x, y: input_y})
+        sess.run(optimization_op, feed_dict={x: input_x, y: input_y})
