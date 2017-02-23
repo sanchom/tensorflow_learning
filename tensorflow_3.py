8c7d6e82dbd3f6b2d54289ea31c3371f4ea272dc
Sancho McCann <sanchom@gmail.com>
2017-02-22 22:48:44 -0800

    Loads data from file and builds sequences of chars.

--- tensorflow_3.py
+++ tensorflow_3.py
@@ -9,27 +9,44 @@ from __future__ import print_function
 
 from six.moves import xrange
 
+import glob
+import random
 import shutil
 import numpy as np
 import tensorflow as tf
 
-NUM_NEURONS = 32
-MAX_LENGTH = 50
-VOCAB_SIZE = 27
+NUM_NEURONS = 128
+MAX_LENGTH = 200
+BATCH_SIZE = 16
 
-def get_dummy_data():
-    # The dummy data is just a small sequence that repeats.
-    sequence = [2, 5, 9, 13, 3, 6, 1, 25, 3, 5, 9, 24]
-    data = np.zeros((1, MAX_LENGTH, VOCAB_SIZE), np.float32)
-    for i in xrange(MAX_LENGTH):
-        data[0,i,sequence[i%len(sequence)]] = 1
-    targets = np.zeros((1, MAX_LENGTH), np.int32)
-    # The target is just the input sequence shifted by one. I.e. the
-    # network is learning to predict the next number in the sequence.
-    # But, the targets are represented simply by integers, not 1-of-k.
-    for i in xrange(MAX_LENGTH):
-        targets[0,i] = sequence[(i+1)%len(sequence)]
-    return data, targets
+class Data(object):
+    def __init__(self):
+        self.data = ''
+        for p in glob.glob('/Users/sanchom/Desktop/whatsapp*'):
+            with open(p) as f:
+                self.data += f.read()
+        self.vocab = sorted(list(set(self.data)))
+        self.vocab_size = len(self.vocab)
+        self.int_data = []
+        for d in self.data:
+            self.int_data.append(self.vocab.index(d))
+
+    def get_next_batch(self):
+        data = np.zeros((BATCH_SIZE, MAX_LENGTH, self.vocab_size), np.float32)
+        starting_ids = []
+        for b in xrange(BATCH_SIZE):
+            starting_ids.append(random.randint(0, len(self.data) - MAX_LENGTH - 2))
+        for b in xrange(BATCH_SIZE):
+            for i in xrange(MAX_LENGTH):
+                data[b,i,self.int_data[starting_ids[b] + i]] = 1
+        targets = np.zeros((BATCH_SIZE, MAX_LENGTH), np.int32)
+        # The target is just the input sequence shifted by one. I.e. the
+        # network is learning to predict the next number in the sequence.
+        # But, the targets are represented simply by integers, not 1-of-k.
+        for b in xrange(BATCH_SIZE):
+            for i in xrange(MAX_LENGTH):
+                targets[b,i] = self.int_data[starting_ids[b] + i + 1]
+        return data, targets
 
 def optimizer(loss_op, global_step):
     with tf.variable_scope('optimizer'):
@@ -40,13 +57,14 @@ def optimizer(loss_op, global_step):
             global_step=global_step)
         return optimize_op
 
-def inputs():
+def inputs(vocab_size):
     with tf.variable_scope('inputs'):
-        data = tf.placeholder(tf.float32, [1, MAX_LENGTH, VOCAB_SIZE])
-        targets = tf.placeholder(tf.int32, [1, MAX_LENGTH])
+        data = tf.placeholder(tf.float32, [None, MAX_LENGTH, vocab_size])
+        targets = tf.placeholder(tf.int32, [None, MAX_LENGTH])
         return data, targets
 
-def inference(data):
+# Vocab size can be inferred from data
+def inference(data, vocab_size):
     with tf.variable_scope('rnn'):
         # Defining the recurrent cell
         cell = tf.contrib.rnn.LSTMCell(NUM_NEURONS)
@@ -58,12 +76,12 @@ def inference(data):
         # in this reshaped tensor.
         output = tf.reshape(output, [-1, NUM_NEURONS])
 
-        output_weights = tf.Variable(tf.truncated_normal([NUM_NEURONS, VOCAB_SIZE], stddev=0.1), name='output_weights')
-        output_bias = tf.Variable(tf.constant(0.1, shape=[VOCAB_SIZE]), name='output_bias')
+        output_weights = tf.Variable(tf.truncated_normal([NUM_NEURONS, vocab_size], stddev=0.1), name='output_weights')
+        output_bias = tf.Variable(tf.constant(0.1, shape=[vocab_size]), name='output_bias')
 
         logits = tf.matmul(output, output_weights) + output_bias
         # Folding the predictions back into sequences of MAX_LENGTH
-        logits = tf.reshape(logits, [-1, MAX_LENGTH, VOCAB_SIZE])
+        logits = tf.reshape(logits, [-1, MAX_LENGTH, vocab_size])
         return logits
 
 def loss(logits, targets):
@@ -72,12 +90,14 @@ def loss(logits, targets):
     tf.summary.scalar('loss', sequence_loss)
     return sequence_loss
 
+data = Data()
+
 # Define the global step and its initialization.
 global_step = tf.Variable(0, name='global_step', trainable=False)
 
 # Putting the graph together.
-input_sequence, target_sequence = inputs()
-logit_sequence = inference(input_sequence)
+input_sequence, target_sequence = inputs(data.vocab_size)
+logit_sequence = inference(input_sequence, data.vocab_size)
 loss_op = loss(logit_sequence, target_sequence)
 optimization_op = optimizer(loss_op, global_step)
 
@@ -93,6 +113,6 @@ with tf.train.MonitoredTrainingSession(
     while not sess.should_stop():
         if (step % 100 == 0):
             print('Step {}'.format(step))
-        dummy_data, dummy_targets = get_dummy_data()
+        dummy_data, dummy_targets = data.get_next_batch()
         sess.run(optimization_op, feed_dict={input_sequence:dummy_data, target_sequence:dummy_targets})
         step += 1
